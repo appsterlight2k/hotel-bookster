b835f9b3922274836d9ea3bed403b1427d717a26
appsterlight <tempreg2k@gmail.com[A[B>
2023-02-17 23:28:49 +0200

    Add Field class

---
+++ src/main/java/com/appsterlight/db/Field.java
@@ -0,0 +1,14 @@
+package com.appsterlight.db;
+
+public class Field {
+    public static final String ID = "id";
+
+    public static final String USER_FIRST_NAME = "first_name";
+    public static final String USER_LAST_NAME= "last_name";
+    public static final String USER_EMAIL= "email";
+    public static final String USER_PHONE_NUMBER = "phone_number";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_ROLE = "role";
+    
+
+}
