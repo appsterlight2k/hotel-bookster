a6f5ea62dc2e12215de1c9e20fe52db19eae779a
appsterlight <tempreg2k@gmail.com[A[B>
2023-02-18 01:30:39 +0200

    Add changes into PropertiesException

---
+++ src/main/java/com/appsterlight/exceptions/PropertiesException.java
@@ -0,0 +1,11 @@
+package com.appsterlight.exceptions;
+
+public class PropertiesException extends GeneralException {
+    public PropertiesException(String message) {
+        super(message);
+    }
+
+    public PropertiesException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
