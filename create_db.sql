176337b4c0776aefc5b1240f810e9cae893d8ac6
appsterlight <tempreg2k@gmail.com[A[B>
2023-02-18 16:52:58 +0200

    small changes in MySQLDBManager and DB script

--- create_db.sql
+++ create_db.sql
@@ -2,19 +2,19 @@ CREATE SCHEMA IF NOT EXISTS hotel DEFAULT CHARACTER SET utf8;
 
 use hotel;
 
-DROP TABLE IF EXISTS account;
-CREATE TABLE user (
-                        id INT UNIQUE NOT NULL AUTO_INCREMENT,
-                        first_name VARCHAR(255) NOT NULL,
-                        last_name VARCHAR(255) NOT NULL,
-                        email VARCHAR(255) NOT NULL,
-                        phone_number VARCHAR(255),
-                        password VARCHAR(255) NOT NULL,
-                        role VARCHAR(255) NOT NULL,
-                        PRIMARY KEY (id)
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+                       id INT UNIQUE NOT NULL AUTO_INCREMENT,
+                       first_name VARCHAR(255) NOT NULL,
+                       last_name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL,
+                       phone_number VARCHAR(255),
+                       password VARCHAR(255) NOT NULL,
+                       role VARCHAR(255) NOT NULL,
+                       PRIMARY KEY (id)
 );
 
-INSERT INTO user (id, first_name, last_name, email, phone_number, password, role)
+INSERT INTO users (id, first_name, last_name, email, phone_number, password, role)
 VALUES
     (1, 'Stephan', 'Rockwell', 'st@gmail.com', '+193520435634', 'LightFlairs', 'ROLE_ADMIN'),
     (2, 'Andrew', 'Hetman', 'ah@gmail.com', '+380969826345', 'Tales', 'ROLE_USER'),
--- src/main/java/com/appsterlight/db/connection/MySQLDBManager.java
+++ src/main/java/com/appsterlight/db/connection/MySQLDBManager.java
@@ -3,8 +3,10 @@ package com.appsterlight.db.connection;
 import com.appsterlight.Constants;
 import com.appsterlight.exceptions.PropertiesException;
 import com.appsterlight.exceptions.DBException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -12,19 +14,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+@Slf4j
 public class MySQLDBManager extends AbstractDBManager {
     private static MySQLDBManager instance = null;
-
-    private MySQLDBManager() {}
-
-    @Override
-    public Connection getConnection() {
-        String URL = getProperties();
+    private MySQLDBManager() {
         try {
-            return DriverManager.getConnection(URL);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            log.error("Can't load BD driver: " + e.getMessage());
+            throw new DBException("Can't load BD driver", e);
+        }
+    }
+
+    public Connection getConnection() {
+        String URL = getURLFromProperties();
+        try {
+            return  DriverManager.getConnection(URL);
         } catch (SQLException e) {
+            log.error(String.format("Connection Exception: Can't establish connection to database %s", e.getMessage()));
             throw new DBException("Can't establish connection to database", e);
-            //LOG.error()
         }
     }
 
@@ -35,21 +43,19 @@ public class MySQLDBManager extends AbstractDBManager {
         return instance;
     }
 
-    private static String getProperties()  {
+    private static String getURLFromProperties()  {
         Properties appProps = new Properties();
-        try (InputStream propFile = new FileInputStream(Constants.SETTINGS_FILE)) {
+        try (InputStream propFile = MySQLDBManager.class.getClassLoader().getResourceAsStream(Constants.SETTINGS_FILE)) {
             appProps.load(propFile);
             String URL = appProps.getProperty("connection.url");
             String user = appProps.getProperty("connection.username");
             String pass =  appProps.getProperty("connection.password");
+
             return URL + "?user=" + user + "&password=" + pass;
-
-//            connection.url=jdbc:mysql://localhost:3306/hotel?user=root&password=root
         } catch (IOException e) {
+            log.error("Application properties Exception: Can't read app.properties from file", e);
             throw new PropertiesException("Can't read app.properties from file; " + e);
-            //LOG.error()...
         }
-
 
     }
 
