f06b40d4b30fdef15e3dd0860b77cdf1f3a9822d
appsterlight <tempreg2k@gmail.com[A[B>
2023-02-20 01:27:24 +0200

    add Messages class with error messages

---
+++ src/main/java/com/appsterlight/controller/Messages.java
@@ -0,0 +1,9 @@
+package com.appsterlight.controller;
+
+public class Messages {
+    public static final String INSERT_ERROR = "Can't insert record into table";
+    public static final String UPDATE_ERROR = "Can't update record in table";
+    public static final String DELETE_ERROR = "Can't delete record from table";
+    public static final String READ_ERROR = "Can't get record from table";
+
+}
