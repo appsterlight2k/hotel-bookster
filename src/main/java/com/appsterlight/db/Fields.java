7260a1f78c8f8c726dc5455fee2457a8a15fcefa
appsterlight <tempreg2k@gmail.com[A[B>
2023-02-20 17:43:54 +0200

    add fields for tables booking and apartment

--- src/main/java/com/appsterlight/db/Fields.java
+++ src/main/java/com/appsterlight/db/Fields.java
@@ -13,8 +13,33 @@ public class Fields {
     public static final String USER_PHONE_NUMBER = "phone_number";
     public static final String USER_PASSWORD = "password";
     public static final String USER_ROLE = "role";
-    public static final String USER_DESCRIPTION = "description";
 
+
+    /* SPECIFIC FIELDS FOR BOOKING TABLE */
+    public static final String BOOKING_USER_ID = "user_id";
+    public static final String BOOKING_APARTMENT_ID = "apartment_id";
+    public static final String BOOKING_CHECK_IN = "check_out";
+    public static final String BOOKING_CHECK_OUT = "check_in";
+    public static final String BOOKING_ADULTS_NUMBER = "adults_number";
+    public static final String BOOKING_CHILDREN_NUMBER = "children_number";
+    public static final String BOOKING_RESERVATION_TIME = "reservation_time";
+    public static final String BOOKING_IS_APPROVED = "is_approved";
+    public static final String BOOKING_IS_BOOKED = "is_booked";
+    public static final String BOOKING_IS_PAID = "is_paid";
+    public static final String BOOKING_IS_CANCELED = "is_canceled";
+
+
+    /* SPECIFIC FIELDS FOR APARTMENT TABLE */
+    public static final String APARTMENT_NUMBER = "apartment_number";
+    public static final String APARTMENT_ROOMS_COUNT = "rooms_count";
+    public static final String APARTMENT_CLASS_ID = "class_id";
+    public static final String APARTMENT_ADULTS_CAPACITY = "adults_capacity";
+    public static final String APARTMENT_CHILDREN_CAPACITY = "children_capacity";
+    public static final String APARTMENT_PRICE = "price";
+    //DESCRIPTION
+
+
+    //                           *** CONNETCION ***
     /* CONNECTION PROPERTIES FIELDS */
     public static final String DB_URL = "connection.url";
     public static final String DB_USERNAME = "connection.username";
