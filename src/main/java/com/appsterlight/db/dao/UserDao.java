70ccd1d1e6a5ed51fac53359fa273109ff0a0061
appsterlight <tempreg2k@gmail.com[A[B>
2023-02-22 16:12:23 +0200

    DAO Layer Refactored: add: AbstractDao class changed: UserDaoImpl, BookingDaoImpl, ApartmentDaoImpl and ApartmentPhotosDaoImpl + Small changes

--- src/main/java/com/appsterlight/db/dao/UserDao.java
+++ src/main/java/com/appsterlight/db/dao/UserDao.java
@@ -1,13 +1,12 @@
 package com.appsterlight.db.dao;
 
-import com.appsterlight.db.entity.Booking;
+import com.appsterlight.db.dao.Dao;
 import com.appsterlight.db.entity.User;
 import com.appsterlight.exceptions.DaoException;
 
+import java.sql.Connection;
 import java.util.Optional;
 
-
 public interface UserDao extends Dao<User> {
-    Optional<User> getUserByEmail(String email) throws DaoException;
-
+    Optional<User> getUserByEmail(String email, Connection con) throws DaoException;
 }
