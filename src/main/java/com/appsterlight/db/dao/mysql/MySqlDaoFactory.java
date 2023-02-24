c4666be8567fc3658b54f8f3c8e3b2fef6ca1c26
appsterlight <tempreg2k@gmail.com[A[B>
2023-02-24 02:08:44 +0200

    Implements MySqlDaoFactory

---
+++ src/main/java/com/appsterlight/db/dao/mysql/MySqlDaoFactory.java
@@ -0,0 +1,54 @@
+package com.appsterlight.db.dao.mysql;
+
+import com.appsterlight.db.dao.*;
+
+import java.sql.Connection;
+
+public class MySqlDaoFactory extends DaoFactory {
+    private UserDao userDao;
+    private ApartmentDao apartmentDao;
+    private BookingDao bookingDao;
+    private ApartmentPhotosDao apartmentPhotosDao;
+
+    private Connection con;
+
+    public MySqlDaoFactory(Connection con) {
+        this.con = con;
+    }
+
+    @Override
+    public UserDao getUserDao() {
+        if (userDao == null) {
+            userDao = new MySqlUserDao(con);
+        }
+
+        return userDao;
+    }
+
+    @Override
+    public ApartmentDao getApartmentDao() {
+        if (apartmentDao == null) {
+            apartmentDao = new MySqlApartmentDao(con);
+        }
+
+        return apartmentDao;
+    }
+
+    @Override
+    public BookingDao getBookingDao() {
+        if (bookingDao == null) {
+            bookingDao = new MySqlBookingDao(con);
+        }
+
+        return bookingDao;
+    }
+
+    @Override
+    public ApartmentPhotosDao getApartmentPhotosDao() {
+        if (apartmentPhotosDao == null) {
+            apartmentPhotosDao = new MySqlApartmentPhotosDao(con);
+        }
+
+        return apartmentPhotosDao;
+    }
+}
