76b36a66335ca74db56254d90e6f4b34c7bd7c38
appsterlight <tempreg2k@gmail.com[A[B>
2023-03-02 16:44:26 +0200

    changed class HomeAction: implemented redirection to Home page by user role functionality

--- src/main/java/com/appsterlight/controller/action/impl/get/HomeAction.java
+++ src/main/java/com/appsterlight/controller/action/impl/get/HomeAction.java
@@ -1,15 +1,25 @@
 package com.appsterlight.controller.action.impl.get;
 
 import com.appsterlight.controller.action.FrontAction;
+import com.appsterlight.controller.action.utils.ControllerUtils;
+import com.appsterlight.controller.constants.PagesNames;
+import com.appsterlight.controller.dto.UserDto;
+import com.appsterlight.model.domain.Role;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class HomeAction extends FrontAction {
 
     @Override
     public String process(HttpServletRequest req, HttpServletResponse resp) {
+        final HttpSession session = req.getSession();
+        UserDto user = (UserDto) session.getAttribute("loggedUser");
 
-        return "home.jsp";
+        return  user != null ?
+                ControllerUtils.getHomePageByRole(user.getRole()) :
+                PagesNames.PAGE_START;
+
     }
 
 }
