19019b24e520f333ae483cb6480cd66045a412b9
appsterlight <tempreg2k@gmail.com[A[B>
2023-03-02 17:02:12 +0200

    added getApprovedBookings and createEventIsPaid

--- src/main/java/com/appsterlight/model/db/dao/BookingDao.java
+++ src/main/java/com/appsterlight/model/db/dao/BookingDao.java
@@ -1,7 +1,16 @@
 package com.appsterlight.model.db.dao;
 
+import com.appsterlight.exception.DaoException;
 import com.appsterlight.model.domain.Booking;
 import com.appsterlight.shared.Dao;
 
+import java.sql.Connection;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface BookingDao extends Dao<Booking> {
+    List<Optional<Booking>> getApprovedBookings(Long id, LocalDate checkIn, LocalDate checkOut, Connection con) throws DaoException;
+
+    boolean createEventIsPaid(Long id, Connection con ) throws DaoException;
 }
