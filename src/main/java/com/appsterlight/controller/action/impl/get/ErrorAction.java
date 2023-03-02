3ad5b7b8c8f7fab11f405e4ad45817e46b765f2b
appsterlight <tempreg2k@gmail.com[A[B>
2023-03-02 16:43:24 +0200

    changed class ErrorAction: string with page name was replaced to related constant

--- src/main/java/com/appsterlight/controller/action/impl/get/ErrorAction.java
+++ src/main/java/com/appsterlight/controller/action/impl/get/ErrorAction.java
@@ -1,12 +1,13 @@
 package com.appsterlight.controller.action.impl.get;
 
 import com.appsterlight.controller.action.FrontAction;
+import com.appsterlight.controller.constants.PagesNames;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class ErrorAction extends FrontAction {
     @Override
     public String process(HttpServletRequest req, HttpServletResponse resp) {
-        return "error.jsp";
+        return PagesNames.PAGE_ERROR;
     }
 }
