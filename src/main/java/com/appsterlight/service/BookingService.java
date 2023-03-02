904ec8d615c77c503d6e4e5a9a67b1dce40f02f7
appsterlight <tempreg2k@gmail.com[A[B>
2023-03-02 17:03:59 +0200

    added methods: setIsApprovedState setIsBooked setIsPaid setIsCanceled

--- src/main/java/com/appsterlight/service/BookingService.java
+++ src/main/java/com/appsterlight/service/BookingService.java
@@ -13,4 +13,9 @@ public interface BookingService {
     boolean updateBooking(Booking booking) throws ServiceException;
     boolean deleteBooking(Long id) throws ServiceException;
     List<Booking> getAllBookings() throws ServiceException;
+
+    void setIsApprovedState(boolean isApproved, Booking booking) throws ServiceException;
+    void setIsBooked(boolean isBooked, Booking booking) throws ServiceException;
+    void setIsPaid(boolean isPaid, Booking booking) throws ServiceException;
+    void setIsCanceled(boolean isCanceled, Booking booking) throws ServiceException;
 }
