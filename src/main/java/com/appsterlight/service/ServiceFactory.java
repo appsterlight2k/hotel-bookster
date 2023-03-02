0d4eedf9490e1c62eb5a7842ecc1813ab642b6cf
appsterlight <tempreg2k@gmail.com[A[B>
2023-03-02 17:00:59 +0200

    added objects references of ApartmentPhotosService and ApartmentTagsService

--- src/main/java/com/appsterlight/service/ServiceFactory.java
+++ src/main/java/com/appsterlight/service/ServiceFactory.java
@@ -1,9 +1,7 @@
 package com.appsterlight.service;
 
 import com.appsterlight.model.db.dao.DaoFactory;
-import com.appsterlight.service.impl.ApartmentServiceImpl;
-import com.appsterlight.service.impl.BookingServiceImpl;
-import com.appsterlight.service.impl.UserServiceImpl;
+import com.appsterlight.service.impl.*;
 import lombok.Getter;
 
 @Getter
@@ -11,12 +9,15 @@ public class ServiceFactory {
     private final UserService userService;
     private final BookingService bookingService;
     private final ApartmentService apartmentService;
+    private final ApartmentPhotosService apartmentPhotosService;
+    private final ApartmentTagsService apartmentTagsService;
 
     private ServiceFactory(DaoFactory daoFactory) {
         this.userService = new UserServiceImpl(daoFactory.getUserDao());
         this.bookingService = new BookingServiceImpl(daoFactory.getBookingDao());
         this.apartmentService = new ApartmentServiceImpl(daoFactory.getApartmentDao());
-
+        this.apartmentPhotosService = new ApartmentPhotosServiceImpl(daoFactory.getApartmentPhotosDao());
+        this.apartmentTagsService = new ApartmentTagsServiceImpl(daoFactory.getApartmentTagsDao());
     }
     public static ServiceFactory getInstance(DaoFactory daoFactory) {
         return new ServiceFactory(daoFactory);
