e3230e5a9d541a115e93678222fba3de9e08dc69
appsterlight <tempreg2k@gmail.com[A[B>
2023-03-02 16:49:48 +0200

    added class ApartmentDto for transferring selected data to client functionality

---
+++ src/main/java/com/appsterlight/controller/dto/ApartmentDto.java
@@ -0,0 +1,31 @@
+package com.appsterlight.controller.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+
+@Builder
+@Data
+@Getter
+//@EqualsAndHashCode(of = {"apartmentNumber", "roomsCount","classId"})
+public class ApartmentDto implements Serializable {
+
+private Long id;
+private String apartmentNumber;
+private Integer roomsCount;
+private Integer classId;
+private Integer adultsCapacity;
+private Integer childrenCapacity;
+private String mainPhotoUrl;
+private Integer price;
+private String className;
+private String classDescription;
+private String description;
+
+    public Long getId() {
+        return id;
+    }
+}
+
+
