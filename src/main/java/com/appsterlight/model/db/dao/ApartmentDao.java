44696a174d372cd8e01471a8c6d4cb9a6ea96341
appsterlight <tempreg2k@gmail.com[A[B>
2023-03-02 17:06:13 +0200

    added getApartmentByApartmentNumber method

--- src/main/java/com/appsterlight/model/db/dao/ApartmentDao.java
+++ src/main/java/com/appsterlight/model/db/dao/ApartmentDao.java
@@ -1,7 +1,12 @@
 package com.appsterlight.model.db.dao;
 
+import com.appsterlight.exception.DaoException;
 import com.appsterlight.model.domain.Apartment;
 import com.appsterlight.shared.Dao;
 
+import java.util.Optional;
+
 public interface ApartmentDao extends Dao<Apartment> {
+    Optional<Apartment> getApartmentByApartmentNumber(String number) throws DaoException;
+
 }
