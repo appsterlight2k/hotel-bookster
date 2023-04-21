fe7b8d7c701d92226d0536b34312acc2d7170afb
appsterlight <tempreg2k@gmail.com>
2023-04-22 00:19:56 +0300

    add remove deleted pages references

--- src/main/java/com/appsterlight/controller/constants/PagesNames.java
+++ src/main/java/com/appsterlight/controller/constants/PagesNames.java
@@ -4,14 +4,13 @@ public final class PagesNames {
     public static final String PAGE_START = "index.jsp";
     public static final String PAGE_LOGIN = "login.jsp";
     public static final String PAGE_REGISTRATION = "registration.jsp";
-    public static final String PAGE_HOME_GUEST = "guest_home.jsp";
-    public static final String PAGE_HOME_USER = "user_home.jsp";
     public static final String PAGE_MANAGER_HOME = "manager_home.jsp";
-    public static final String PAGE_MANAGER_REQUESTS = "manager_requests.jsp";
+
+    public static final String PAGE_MANAGER_REQUESTS = "manager_requests.jsp"; //Requests for Booking, Booking Requests
+    public static final String PAGE_MANAGER_BOOKED = "manager_booked.jsp";
     public static final String PAGE_CABINET = "cabinet.jsp";
-    public static final String PAGE_APARTMENTS_USER = "user_apartments.jsp";
-    public static final String PAGE_APARTMENT_SHOW = "user_show_apartment.jsp";
-    public static final String PAGE_APARTMENTS_GUEST = "guest_apartments.jsp";
+    public static final String PAGE_APARTMENTS = "apartments.jsp";
+    public static final String PAGE_APARTMENT_SHOW = "show_apartment.jsp";
     public static final String PAGE_ERROR = "error.jsp";
 
 
