7a1708a7d6c3d64462a85a120772bf0d9a64cef9
appsterlight <tempreg2k@gmail.com>
2023-04-22 00:21:17 +0300

    small changes

--- src/main/java/com/appsterlight/controller/action/utils/ControllerUtils.java
+++ src/main/java/com/appsterlight/controller/action/utils/ControllerUtils.java
@@ -1,12 +1,8 @@
 package com.appsterlight.controller.action.utils;
 
 import com.appsterlight.controller.constants.PagesNames;
-import com.appsterlight.controller.dto.ApartmentDto;
-import com.appsterlight.model.domain.Apartment;
 import com.appsterlight.model.domain.Role;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 @Slf4j
 public final class ControllerUtils {
@@ -27,26 +23,4 @@ public final class ControllerUtils {
             return PagesNames.PAGE_START;
         }
     }
-
-    public static String getApartmentsPageByRole(String role) {
-        try {
-            Role actualRole = Role.valueOf(role);
-            if (actualRole == Role.ROLE_MANAGER) {
-                return PagesNames.PAGE_MANAGER_HOME;
-            } else if (actualRole == Role.ROLE_USER) {
-                return PagesNames.PAGE_APARTMENTS_USER;
-            } else {
-                return PagesNames.PAGE_APARTMENTS_GUEST;
-            }
-        } catch (IllegalArgumentException e) {
-            log.error("Can't get home page for role! " + e.getMessage());
-            return PagesNames.PAGE_APARTMENTS_GUEST;
-        }
-    }
-
-
-
-
-
-
 }
